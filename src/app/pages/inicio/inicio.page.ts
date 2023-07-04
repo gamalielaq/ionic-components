fbd93b9f2aa81b2faaeebb569cd515f333d9ee68
= <gabanto@canvia.com>
2023-07-04 14:49:41 -0500

    WIP

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente {
+  icon: string;
+  name:string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+  components: Componente[] = [
+    {icon: 'american-football', name: 'action sheet',redirectTo: '/action-sheet' },
+    {icon: 'alert-circle-outline', name: 'action sheet',redirectTo: '/alert' }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
